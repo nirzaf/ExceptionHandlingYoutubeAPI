a53c368fccec28ed4f0172e5a7569786d5c54b9a
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 09:09:50 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                YouTubeApi api = new YouTubeApi();
+                List<Video> videos = api.GetVideos("mosh");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
